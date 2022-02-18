33efab2864df7f370a46276e46d1bc8c45814ada
vicceledon <vicceledon@gmail.com>
2022-02-17 21:47:19 -0300

    modificacines

--- app.js
+++ app.js
@@ -3,3 +3,4 @@ console.log(nombre);
 let fs = require("fs");
 let archivo = fs.readFileSync("personajes.JSON");
 console.log(archivo);
+console.log("prueba");
--- victor.js
+++ victor.js
@@ -1,2 +1,2 @@
-let dato = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let dato = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 module.exports = dato;
